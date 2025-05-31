41706b3dcd1474a8b0d3de01134e951c3f1423d9
Darya-Rusanova <dr1305@bk.ru>
2025-05-31 23:17:49 +0800

    ...

--- js/userName.js
+++ js/userName.js
@@ -26,7 +26,7 @@ async function checkAuthAndGetUsername() {
         if (response.ok) {
             const userData = await response.json();
             console.log(userData.username);
-            return userData.username; 
+            alert(userData.username); 
         } else {
             throw new Error(`Ошибка HTTP! Статус: ${response.status}`);
         }
