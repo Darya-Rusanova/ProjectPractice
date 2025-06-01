abafdd5157f9576274686ad4a156ce5db86e7478
Darya-Rusanova <dr1305@bk.ru>
2025-06-02 01:11:02 +0800

    уведомление

---
+++ js/showNotif.js
@@ -0,0 +1,21 @@
+function showNotification(message, type = 'error') {
+    const notification = document.getElementById('error');
+
+    notification.classList.remove('show', 'hide');
+    void notification.offsetWidth; // Триггер reflow
+
+    notification.textContent = message;
+    notification.className = `error-notification ${type}`;
+
+    setTimeout(() => {
+      notification.classList.add('show');
+      
+      setTimeout(() => {
+        notification.classList.remove('show');
+        notification.classList.add('hide');
+        setTimeout(() => {
+          notification.classList.remove('hide');
+        }, 500);
+      }, 2000);
+    }, 50);
+  }
